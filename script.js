8be4f4c26dba28a43f536ccdd5ff963be339dacb
dineshkumar-stack <stardinesh4@gmail.com>
2023-02-23 13:23:34 +0530

    Completed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.head.append(title);
 var container = document.createElement("div");
 container.setAttribute("class", "container");
 var row = document.createElement("div");
-row.classList.add("row", "m-3");
+row.classList.add("row");
 container.append(row);
 
 try {
@@ -16,23 +16,29 @@ try {
     Countrydata.filter(async (x) => {
       var weatherAPI = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${x.name}&appid=15eca4ff9fb9f32f93e2e7a6b3785e3c`);
       var weatherData = await weatherAPI.json();
-      //  console.log(weatherData);
+      //  console.log(Countrydata);
       $(function () {
         $('[data-toggle="popover"]').popover()
       })
+      console.log(Countrydata);
+
       row.innerHTML += `
-<div class="card text-center col-md-4">
+<div class="card text-center col-sm-6 col-md-4 col-lg-4 col-xl-4">
+<div class="card h-100">
+
 <div class="card-header"><b>${x.name}</b></div>
 <div class="card-body">
- <img src="${x.flag}" class="card-img-top" alt="...">                            
- <p class="card-text"><b>Capital:</b> ${x.capital}</p>         
- <p class="card-text"> <b>Region:</b> ${x.region}</p> 
- <p class="card-text"> <b>Country Code:</b> ${x.alpha2Code}</p>   
+ <img src="${x.flag}" class="card-img-top" alt="...">  
+ <div class="card-text">                     
+ <p  <b>Capital:</b> ${x.capital}</p>         
+ <p  <b>Region:</b> ${x.region}</p> 
+ <p  <b>Country Code:</b> ${x.alpha2Code}</p> 
+ </div> 
  <button type="button" class="btn btn-secondary" data-container="body" title="Weather:${weatherData.weather[0].description} " data-toggle="popover" data-placement="top"
   data-content="Temp: ${weatherData.main.temp} |
  Pressure: ${weatherData.main.pressure} |
  Humidity: ${weatherData.main.humidity}">
- Click for Weather
+ Weather
  </button>
 </div>
 </div>`;
